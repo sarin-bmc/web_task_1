a73cefc796068dd0fe75dc81f58f41394a389ac2
Sarin M <sarinm@Sarins-MacBook-Air.local>
2023-08-11 12:17:17 +0530

    customised theme for section 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        bmcbg:{
+          200: '#f1edfd',
+          400: '#EEE9FC',
+          600: '#e4dbfa',
+        },
+        bmc:{
+          'yellow': '#FFDD00',
+          'fuchsia': '#ce30ff',
+          'indigo': '#667EF7',
+          'orange': '#F66140',
+        }
+      }
+    },
   },
   plugins: [],
 }
