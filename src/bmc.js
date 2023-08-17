383dcded2678b3f887aa874f3a392e0d44ce6c19
Sarin M <sarin@buymeacoffee.com>
2023-08-17 12:43:07 +0530

    faq toggle to jquery

--- src/bmc.js
+++ src/bmc.js
@@ -1,23 +1,3 @@
-// FAQ toggle
-const toggleFaq = (boxId) => {
-  var x = document.getElementById(boxId);
-  box = document.getElementById(boxId);
-  description = box.getElementsByClassName("faq-box-description-wrapper")[0];
-  toggleButton = box.getElementsByClassName('faq-box-header')[0].getElementsByClassName('faq-box-header-toggle')[0];
-  
-  if (description.classList.contains("hidden")) {
-    description.classList.remove("hidden");
-    toggleButton.getElementsByClassName("faq-button-minus")[0].classList.remove("hidden");
-    toggleButton.getElementsByClassName("faq-button-add")[0].classList.add("hidden");
-    return;
-  }
-  
-  description.classList.add("hidden");
-  toggleButton.getElementsByClassName("faq-button-minus")[0].classList.add("hidden");
-  toggleButton.getElementsByClassName("faq-button-add")[0].classList.remove("hidden");
-  return;
-};
-
 // nav toggle for mobile view
 const toggleNav = ()=>{
   var toggleIcon = document.getElementById('toggle_icon');
@@ -25,14 +5,10 @@ const toggleNav = ()=>{
   if(navbar.classList.contains("hidden")){
     navbar.classList.remove('hidden')
     toggleIcon.classList.add('hidden')
-    toggleIcon.getElementsByClassName('toggle-arrow')[0].classList.remove('rotate-90')
-    toggleIcon.getElementsByClassName('toggle-arrow')[0].classList.add('-rotate-90')
     return;
   }
   navbar.classList.add('hidden')
   toggleIcon.classList.remove('hidden')
-    toggleIcon.getElementsByClassName('toggle-arrow')[0].classList.add('rotate-90')
-    toggleIcon.getElementsByClassName('toggle-arrow')[0].classList.remove('-rotate-90')
     return;
 }
 
---
+++ src/jquery.js
@@ -0,0 +1,15 @@
+
+$(function(){
+    $('#faq-box-1-button').on("click",()=>{
+        $('#faq-box-1').slideToggle(300);
+    })
+    $('#faq-box-2-button').on("click",()=>{
+        $('#faq-box-2').slideToggle(300);
+    })
+    $('#faq-box-3-button').on("click",()=>{
+        $('#faq-box-3').slideToggle(300);
+    })
+    $('#faq-box-4-button').on("click",()=>{
+        $('#faq-box-4').slideToggle(300);
+    })
+});
